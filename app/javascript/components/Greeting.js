e362c38b5a076333813f71a3879fcdfc329467e7
PNdunguMaina <ndunguwamaina@gmail.com>
2022-12-14 14:54:26 +0300

    Create Greeting component

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../redux/greetings/greeting';
 
-const Greeting = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+function Greeting() {
+  const greetings = useSelector((state) => state.greetings);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, []);
+  return (
+    <div>
+      <h2>{greetings}</h2>
+      <button
+        onClick={() => {
+          dispatch(fetchGreeting());
+        }}
+      >
+        Show Greeting
+      </button>
+    </div>
+  );
+}
 
 export default Greeting;
